15f7a1e5578432cf019196153a9c70c24e79ffef
daniyal karwnd <karwnddaniyal16@gmail.com>
2024-04-23 11:33:05 +0330

    dynamically in the url we git a name then display it on the users web page like facebook

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome dani"
+@app.route("/user/<name>")
+def home(name):
+    return "<h1>Welcome, %s! </h1>" % name
