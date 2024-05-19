ade5f1cb1ebf27db87c0f18e686b3f2da7bffa5f
daniyal karwnd <karwnddaniyal16@gmail.com>
2024-05-19 19:39:43 +0330

    we get an input from the user to enter his name and every series of valuse hr enters will be deleted

--- app.py
+++ app.py
@@ -1,73 +1,21 @@
-from datetime import datetime
 from flask import Flask
+from forms import NameForm
 from flask import render_template
 from flask_bootstrap import Bootstrap
-from flask import url_for
-from flask import request
-from flask_moment import Moment
-
-
 
 app = Flask(__name__)
+
+
+from app import app
 bootstrap = Bootstrap(app)
-moment = Moment(app)
-app.config["SECRET_KEY"] = "hi dani hello your is is welcome "
+app.config ["SECRET_KEY"] = "WElcome To Web"
 
 
-@app.route("/")
-def page_one():
-    return render_template("serch.html")
-
-
-@app.route("/home")
-def home_page():
-    time = datetime.utcnow()
-    return render_template("home.html", time = time)
-
-
-@app.route("/home/<user>")
-def home_page2(user):
-    time = datetime.utcnow()
-    return render_template("home.html", user= user, time=time)
-
-
-
-@app.route("/aboutus")
-def aboutus():
-    return render_template("aboutus.html")
-
-@app.route("/usersystemprofile")
-def user_pofile():
-    agent = request.headers.get("user_agent")
-    return render_template("usersystemprofile.html", agent=agent)
-
-
-@app.route("/comments")
-def comment():
-        user = {"name": "dani"}
-        posts = [
-        {
-            'author': {"username":"neda"},
-            'body':"beautiful day in portland"
-         },
-
-         {
-            'author':{"username":"ali"},
-            'body': "the avengers movie was os cool !"
-          }
-    ]
-        return render_template("comments.html",user = user, posts = posts)
-    
-
-@app.errorhandler(404)
-def Error_404(e):
-    return render_template("404.html", title = "error 404"), 404
-
-
-@app.errorhandler(500)
-def Error500(e):
-    
-    return render_template("500.html", title = "error 500"), 500
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/", methods=["POST", "GET"])
+def home():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = " "
+    return  render_template("home.html", form = form, name = name)
