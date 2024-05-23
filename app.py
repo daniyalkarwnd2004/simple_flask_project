a733437d6ecc7d966600cb617726f1be09f7cef8
daniyal karwnd <karwnddaniyal16@gmail.com>
2024-05-23 18:17:14 +0330

    project

--- app.py
+++ app.py
@@ -1,21 +1,46 @@
 from flask import Flask
-from forms import NameForm
 from flask import render_template
+from datetime import datetime
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment 
+
 
 app = Flask(__name__)
-
-
 from app import app
 bootstrap = Bootstrap(app)
-app.config ["SECRET_KEY"] = "WElcome To Web"
+moment = Moment(app)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = " "
-    return  render_template("home.html", form = form, name = name)
+    time = datetime.utcnow()
+    return render_template("home.html", time = time)
+
+@app.route("/song")
+def song():
+    time = datetime.utcnow()
+    return render_template("happy_song.html", time=time)
+
+@app.route("/special")
+def special():
+    time = datetime.utcnow()
+    return render_template("special_songs.html", time=time)
+
+
+@app.route("/remex")
+def remex():
+    time = datetime.utcnow()
+    return render_template("remex.html", time = time)
+
+@app.errorhandler(404)
+def Error_404(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def Error_500():
+    return render_template("500.html"), 500
+
+
+
+if __name__ == "main":
+    app.run(debug=True)
